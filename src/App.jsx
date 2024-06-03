bdcf8d8c81958120a3a57087653acc02ed7d943d
Madhusudan0903 <madhusudan09mss@gmail.com>
2024-06-03 13:56:54 +0530

    Updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,4 @@ const App = () => {
   )
 }
 export default App
+//"homepage":" https://madhusudan0903.github.io/first-react-project",  - jab bhi react application ko github pages pe github actions se pe deploy krte ho to ye homepage naam ki property add krni pdti hai jisme link aata hai vo website ka
